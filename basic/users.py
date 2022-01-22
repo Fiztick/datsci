78f3bdaa8ffb5a9037780dde3ec5f013c50799d3
bassamtiano <bassamtiano@gmail.com>
2022-01-22 14:54:24 +0700

    Tambah Random

--- basic/users.py
+++ basic/users.py
@@ -1,3 +1,5 @@
+from random import random
+
 class User:
 
     def __init__(self, keys):
@@ -5,14 +7,31 @@ class User:
 
     def pertambahan(self, data):
         result = []
-        for baris in data:
-            result.append({"bagus": baris + self.key})
+        for i, item in enumerate(data):
+            hasil = item + self.key
+            result.append({"bagus": hasil})
+
         return result
 
     def pembagian(self, data):
         result = []
         for i, baris in enumerate(data):
             result.append(baris / self.key)
+        return result
+
+    def random_max(self, data):
+        result = []
+        maxs = 0
+
+        for i, baris in enumerate(data):
+            # hit = random() * baris
+            result.append(baris)
+
+            if baris > maxs:
+                maxs = baris
+
+        print("Nilai maksimum : ", maxs)
+
         return result
 
 
--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 #  Import Class User di Folder basic
 from basic.users import User
 
+#  Import Functions di Folder basic
 from basic.methodbase import biodata
 
 def main():
     datas = [
-        1, 2, 3, 4, 6
+        1, 2, 3, 4, 6, 7, 1, 3, 40, 100, 312, 3125, 64, 21
     ]
 
     users = User(keys = 3)
     result = users.pertambahan(datas)
 
-    return result
+    random_result = users.random_max(datas)
+
+    return result, random_result
 
 if __name__ == '__main__':
-    result_pertambahan = main()
+    result_pertambahan, result_random = main()
     
-    for i, hasil in enumerate(result_pertambahan):
-        print(hasil["bagus"])
+    # for i, hasil in enumerate(result_pertambahan):
+    #     print(hasil["bagus"])
 
-    bio = biodata()    
+    # bio = biodata()    
 
-    for i, baris in enumerate(bio):
-        print((i + 1), "|", baris["nama"], "|", baris["alamat"])
+    # for i, baris in enumerate(bio):
+    #     print((i + 1), "|", baris["nama"], "|", baris["alamat"])
 
-        # for key in baris:
-        #     print(key, "=", baris[key])
+    #     # for key in baris:
+    #     #     print(key, "=", baris[key])
+
+    for baris in result_random:
+        print(baris)
